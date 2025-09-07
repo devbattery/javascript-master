af80a555ebb3dbce09ffd11dfabb6a1f53b24e20
devbattery <devbattery@outlook.com>
2025-09-08 00:02:15 +0900

    Feat[JavaScript Fundamentals - Part 1]: The Conditional (Ternary) Operator

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -218,7 +218,6 @@ if (hasDriversLicense && hasGoodVision && !isTired) {
 } else {
   console.log("Someone else should drive...");
 }
-*/
 
 const day = "monday";
 
@@ -231,3 +230,20 @@ switch (day) {
   default:
     console.log("Not a valid day!");
 }
+*/
+
+const age = 23;
+age >= 18
+  ? console.log("I like to drink wine 🍷")
+  : console.log("I like to drink water 💧");
+
+let drink2;
+if (age >= 18) {
+  drink2 = "wine";
+} else {
+  drink2 = "water";
+}
+
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? "wine" : "water"}`);

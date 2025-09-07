eef4951b343b41c3ee6c71317ed11ed8d412b02f
devbattery <devbattery@outlook.com>
2025-09-08 08:00:59 +0900

    Feat[JavaScript Fundamentals - Part 2]: Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,3 +1,4 @@
+/*
 "use strict";
 
 let hasDriversLicense = false;
@@ -8,3 +9,28 @@ if (hasDriversLicense) console.log(`I can drive :D`);
 
 // const interface = "Audio";
 // const private = 534;
+*/
+
+"use strict";
+
+function logger() {
+  console.log(`My name is Jonas`);
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(applesNum, orangesNum) {
+  const juice = `Juice with ${applesNum} apples and ${orangesNum} oranges`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number("23");

1065d2b6709b39c9c04c0bef1db6148c39d01207
devbattery <devbattery@outlook.com>
2025-09-07 23:08:59 +0900

    Feat[JavaScript Fundamentals - Part 1]: Operator Precedence

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -56,7 +56,6 @@ job = "teacher";
 
 lastName = "Schemedtmann";
 console.log(lastName);
-*/
 
 const now = 2037;
 const ageJonas = now - 1991;
@@ -82,4 +81,19 @@ console.log(ageSarah >= 18);
 const isFullAge = ageSarah >= 18;
 
 console.log(now - 1991 > now - 2018);
+*/
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah);
+console.log(averageAge);
 

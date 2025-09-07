2bcb00169e2a158709b8dd578b61723d06f52434
devbattery <devbattery@outlook.com>
2025-09-07 23:23:12 +0900

    Feat[JavaScript Fundamentals - Part 1]: if / else Statements

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -95,7 +95,6 @@ console.log(x, y);
 const averageAge = (ageJonas + ageSarah) / 2;
 console.log(ageJonas, ageSarah);
 console.log(averageAge);
-*/
 
 const firstName = "Jonas";
 const job = "teacher";
@@ -122,3 +121,26 @@ console.log(
 multiple
 lines`
 );
+*/
+
+const age = 19;
+const isOldEnough = age >= 18;
+
+if (isOldEnough) {
+  console.log(`Sarah can start driving license`);
+} else {
+  const yearsLeft = 18 - age;
+  console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
+}
+
+const birthYear = 2012;
+
+let century;
+if (birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+
+console.log(century);
+

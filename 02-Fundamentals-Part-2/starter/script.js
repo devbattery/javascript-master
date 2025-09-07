e94fcb9274ff033e5acfdd0ba3ea36038454601c
devbattery <devbattery@outlook.com>
2025-09-08 08:25:42 +0900

    Feat[JavaScript Fundamentals - Part 2]: Functions Calling Other Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -50,7 +50,6 @@ const calcAge2 = function (birthYear) {
 const age2 = calcAge2(1991);
 
 console.log(age1, age2);
-*/
 
 // Arrow Funtion
 const calcAge3 = (birthYear) => 2037 - birthYear;
@@ -65,3 +64,19 @@ const yearsUntilRetirement = (birthYear, firstName) => {
 };
 console.log(yearsUntilRetirement(1991, `Jonas`));
 console.log(yearsUntilRetirement(1980, `Wonjun`));
+*/
+
+function cutFruitPrieces(fruit) {
+  return fruit * 4;
+}
+
+function fruitProcessor(applesNum, orangesNum) {
+  const applesPieces = cutFruitPrieces(applesNum);
+  const orangePieces = cutFruitPrieces(orangesNum);
+
+  const juice = `Juice with ${applesPieces} piece of apples 
+  and ${orangePieces} pieces of oranges`;
+  return juice;
+}
+
+console.log(fruitProcessor(2, 3));

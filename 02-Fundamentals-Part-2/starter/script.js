d01f82de8ebdfdec0482a30c621d7561ae8953bb
devbattery <devbattery@outlook.com>
2025-09-08 08:14:54 +0900

    Feat[JavaScript Fundamentals - Part 2]: Arrow Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -34,8 +34,6 @@ console.log(appleOrangeJuice);
 
 const num = Number("23");
 
-*/
-
 // Function declaration
 function calcAge1(birthYear) {
   return 2037 - birthYear;
@@ -52,4 +50,18 @@ const calcAge2 = function (birthYear) {
 const age2 = calcAge2(1991);
 
 console.log(age1, age2);
+*/
 
+// Arrow Funtion
+const calcAge3 = (birthYear) => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  //   return retirement;
+  return `${firstName} retires in ${retirement} years`;
+};
+console.log(yearsUntilRetirement(1991, `Jonas`));
+console.log(yearsUntilRetirement(1980, `Wonjun`));

6af8a4c0cd95a4d7fc24880b6e0685e47153addc
devbattery <devbattery@outlook.com>
2025-09-08 15:21:17 +0900

    Feat[JavaScript Fundamentals - Part 2]: Dot vs Bracket Notation

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -155,7 +155,6 @@ console.log(friends.includes(23));
 if (friends.includes("Steven")) {
   console.log("You have a friend called Steven");
 }
-*/
 
 const jonasArray = [
   "Jonas",
@@ -173,4 +172,40 @@ const jonas = {
   friends: ["Michael", "Peter", "Steven"],
 };
 console.log(jonas);
+*/
 
+const jonas = {
+  firstName: "Jonas",
+  lastName: "Schmedtmann",
+  age: 2037 - 1991,
+  job: "teacher",
+  friends: ["Michael", "Peter", "Steven"],
+};
+console.log(jonas);
+
+console.log(jonas.lastName);
+console.log(jonas["lastName"]);
+
+const nameKey = "Name";
+console.log(jonas["first" + nameKey]);
+console.log(jonas["last" + nameKey]);
+
+const interestedIn = prompt(
+  "What do you want to know about Jonas? Choose between firstName, lastName, age, job, and freinds"
+);
+
+if (jonas[interestedIn]) {
+  console.log(jonas[interestedIn]);
+} else {
+  console.log("Wrong request!");
+}
+
+jonas.location = "Portugal";
+jonas["twitter"] = "@jonasschemdtman";
+console.log(jonas);
+
+// Challenge
+// "Jonas has 3 friends, and his best friend is called Michael"
+console.log(
+  `${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`
+);

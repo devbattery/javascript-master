5e542efc395efabe5b4a889b695b5cd89bae5fa5
devbattery <devbattery@outlook.com>
2025-09-09 22:08:33 +0900

    Feat[Data Structures, Modern Operators and Strings]: Optional Chaining

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -69,6 +69,34 @@ const restaurant = {
   },
 };
 
+if (restaurant.openingHours && restaurant.openingHours.mon) {
+  console.log(restaurant.openingHours.mon.open);
+}
+
+// WITH optional chaining
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+// Example
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+for (const day of days) {
+  console.log(day);
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day}, we open at ${open}`);
+}
+
+// Methods
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+console.log(restaurant.orderResiotto?.(0, 1) ?? 'Method does not exist');
+
+// Arrays
+const users = [
+  { name: 'Jonas', email: 'hello@jonas.io' },
+  { name: 'Wonjun', email: 'hello@wonjun.io' },
+];
+console.log(users[1]?.name ?? 'User array empty');
+
+/*
 const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
 for (const item of menu) {
@@ -81,7 +109,6 @@ for (const [i, el] of menu.entries()) {
 
 // console.log([...menu.entries()]);
 
-/*
 // SPREAD, because on RIGHT side of =
 const arr = [1, 2, ...[3, 4]];
 

0a75663a7388f30584540754b573f68803159e21
devbattery <devbattery@outlook.com>
2025-09-10 20:40:54 +0900

    Feat[A Closer Look at Functions]: The bind Method

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -44,3 +44,45 @@ const flightData = [583, 'George Cooper'];
 book.apply(swiss, flightData);
 // book.call(swiss, ...flightData); <- Same
 console.log(swiss);
+
+// Bind method
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+
+bookEW(23, 'Steven Williams');
+
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23('Jonas Schmedtmann');
+bookEW23('Martha Cooper');
+
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+  this.planes++;
+  console.log(this.planes);
+};
+// lufthansa.buyPlane();
+
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(10, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+// addVAT = (value) => value + value * 0.23;
+
+console.log(addVAT(100));
+console.log(addVAT(23));
+
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(100));
+console.log(addVAT2(23));

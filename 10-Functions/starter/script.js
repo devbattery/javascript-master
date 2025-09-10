2138e818cbbd8e30f0f1d07e981cc99734502089
devbattery <devbattery@outlook.com>
2025-09-10 21:33:32 +0900

    Feat[A Closer Look at Functions]: More Closure Examples

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -110,7 +110,6 @@ runOnce();
 
 // console.log(isPrivate);
 console.log(notPrivate);
-*/
 
 const secureBooking = function () {
   let passengerCount = 0;
@@ -126,3 +125,44 @@ const booker = secureBooking();
 booker();
 booker();
 booker();
+*/
+
+// Ex 1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+console.dir(f);
+
+// Re-assinging f function
+h();
+f();
+
+// Ex 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are not boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+const perGroup = 1000;
+boardPassengers(180, 3);

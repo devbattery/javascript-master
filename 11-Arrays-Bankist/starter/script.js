219f0e4fb63996d7c8de16291a5c3747114debaa
devbattery <devbattery@outlook.com>
2025-09-12 17:15:41 +0900

    Feat[Working With Arrays]: The New findLast and findLastIndex Methods

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -286,3 +286,17 @@ console.log(accounts);
 const account = accounts.find((acc) => acc.owner === 'Jessica Davis');
 console.log(account);
 */
+
+console.log(movements);
+const lastWithdrawal = movements.findLast((mov) => mov < 0);
+console.log(lastWithdrawal);
+
+const lastestLargeMovementIndex = movements.findLastIndex(
+  (mov) => Math.abs(mov) > 1000
+);
+console.log(lastestLargeMovementIndex);
+console.log(
+  `Your latest large movement was ${
+    movements.length - lastestLargeMovementIndex
+  } movements ago`
+);

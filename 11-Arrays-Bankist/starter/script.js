5ad4da6a8570487fdcfcfbfca20ddbf49ba49caf
devbattery <devbattery@outlook.com>
2025-09-12 17:47:02 +0900

    Feat[Working With Arrays]: flat and flatMap

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -316,7 +316,6 @@ console.log(
     movements.length - lastestLargeMovementIndex
   } movements ago`
 );
-*/
 
 console.log(movements);
 
@@ -335,4 +334,23 @@ const deposit = (mov) => mov > 0;
 console.log(movements.some(deposit));
 console.log(movements.every(deposit));
 console.log(movements.filter(deposit));
+*/
 
+const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
+console.log(arr.flat());
+
+const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
+console.log(arrDeep.flat(2));
+
+// flat
+const overalBalance = accounts
+  .map((acc) => acc.movements)
+  .flat()
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overalBalance);
+
+// flatMap
+const overalBalance2 = accounts
+  .flatMap((acc) => acc.movements)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overalBalance);

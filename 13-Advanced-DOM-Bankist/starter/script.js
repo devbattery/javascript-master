8d57b888674314109dca3e4a0ac3c967d25c0c67
devbattery <devbattery@outlook.com>
2025-09-16 19:54:33 +0900

    Feat[Advanced DOM and Events]: Building a Tabbed Component

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -92,6 +92,32 @@ document.querySelector('.nav__links').addEventListener('click', (e) => {
   }
 });
 
+// Tabbed component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+  console.log(clicked);
+
+  // Guard clause
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach((t) => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach((c) => c.classList.remove('operations__content--active'));
+
+  // Active tab
+  clicked.classList.add('operations__tab--active');
+
+  // Active content area
+  console.log(clicked.dataset.tab);
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
+
 ///////////////LECTURE///////////////
 
 /*
@@ -253,7 +279,6 @@ document.querySelector('.nav').addEventListener('click', function (e) {
   this.style.backgroundColor = randomColor();
   console.log('NAV', e.target, e.currentTarget);
 });
-*/
 
 const h1 = document.querySelector('h1');
 
@@ -282,3 +307,4 @@ console.log(h1.parentElement.children);
 [...h1.parentElement.children].forEach((el) => {
   if (el != h1) el.style.transform = 'scale(0.5)';
 });
+*/

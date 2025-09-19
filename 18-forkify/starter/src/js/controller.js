044141cca42adecb5cc34c0f86b22831647badfd
devbattery <devbattery@outlook.com>
2025-09-19 18:21:54 +0900

    Feat[Forkify App: Building a Modern Application]: Listening  For load and hashchange Events

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -34,11 +34,16 @@ const renderSpinner = function (parentEl) {
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
+
+    if (!id) return;
+
     // 1) Loading recipe
     renderSpinner(recipeContainer);
 
     const res = await fetch(
-      'https://forkify-api.jonas.io/api/v2/recipes/664c8f193e7aa067e94e8704'
+      `https://forkify-api.jonas.io/api/v2/recipes/${id}`
       // 'https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886'
     );
     const data = await res.json();
@@ -167,4 +172,4 @@ const showRecipe = async function () {
   }
 };
 
-showRecipe();
+['hashchange', 'load'].forEach((ev) => window.addEventListener(ev, showRecipe));
